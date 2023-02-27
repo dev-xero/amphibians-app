80e798eb80b12d18a490d5c6666dd41bc6f41e92
Xero kun <elo48858@gmail.com>
2023-02-27 19:07:26 +0100

    `✨UPDATE` Created Amphibian Info Card

--- app/src/main/java/dev/xero/amphibians/model/AmphibianData.kt
+++ app/src/main/java/dev/xero/amphibians/model/AmphibianData.kt
@@ -1,7 +1,10 @@
 package dev.xero.amphibians.model
 
+import androidx.annotation.DrawableRes
+
 data class AmphibianData(
 	val title: String?,
 	val content: String?,
-	val imgSrc: String?
+	/*TODO: CHANGE THIS TO STRING LATER*/
+	@DrawableRes val imgSrc: Int? = null
 )
