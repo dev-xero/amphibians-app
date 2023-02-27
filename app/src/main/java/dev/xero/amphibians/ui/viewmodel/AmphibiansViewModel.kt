74871f7e9740e5865435abae4b5b7af171961da0
Xero kun <elo48858@gmail.com>
2023-02-27 18:39:43 +0100

    `✨UPDATE` Setup UI State as State Flow

--- app/src/main/java/dev/xero/amphibians/ui/viewmodel/AmphibiansViewModel.kt
+++ app/src/main/java/dev/xero/amphibians/ui/viewmodel/AmphibiansViewModel.kt
@@ -1,4 +1,13 @@
 package dev.xero.amphibians.ui.viewmodel
 
-class AmphibiansViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class AmphibiansViewModel : ViewModel() {
+
+	private val _uiState = MutableStateFlow(AmphibiansUiState())
+	val uiStateFlow: StateFlow<AmphibiansUiState> = _uiState.asStateFlow()
+
 }
