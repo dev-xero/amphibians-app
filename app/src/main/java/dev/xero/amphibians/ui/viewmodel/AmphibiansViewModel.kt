97f966c8574972c583694c2185fd11b832b2561b
Xero kun <elo48858@gmail.com>
2023-02-27 20:59:17 +0100

    `✨UPDATE` Updated View Model to Call the API on INIT

--- app/src/main/java/dev/xero/amphibians/ui/viewmodel/AmphibiansViewModel.kt
+++ app/src/main/java/dev/xero/amphibians/ui/viewmodel/AmphibiansViewModel.kt
@@ -1,23 +1,46 @@
 package dev.xero.amphibians.ui.viewmodel
 
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import dev.xero.amphibians.AmphibiansApplication
 import dev.xero.amphibians.model.AmphibianData
 import dev.xero.amphibians.repository.AmphibianRepositoryImplementation
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import okio.IOException
 
 class AmphibiansViewModel(
 	private val amphibiansRepository: AmphibianRepositoryImplementation
 ) : ViewModel() {
 
-	private val _uiState = MutableStateFlow(AmphibiansUiState())
-	val uiState: StateFlow<AmphibiansUiState> = _uiState.asStateFlow()
+	private val TAG = "APP"
+
+	var _uiState: AmphibiansUiState by mutableStateOf(AmphibiansUiState.Loading)
+		private set
+
+	// ATTEMPT TO FETCH THE DATA
+	init {
+		getAmphibians()
+	}
+
+	 private fun getAmphibians() {
+		 viewModelScope.launch {
+			 _uiState = try {
+				 AmphibiansUiState.Success(data = amphibiansRepository.getAmphibianData())
+			 } catch (e: IOException) {
+				 Log.d(TAG, e.toString())
+				 AmphibiansUiState.Failure
+			 }
+		 }
+	}
+
 
 	// Factory for the viewModel
 	companion object {
