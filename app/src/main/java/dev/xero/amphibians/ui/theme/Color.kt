ac117a5d90e6b167fea4d706eb4bad44f4165bbc
Xero kun <elo48858@gmail.com>
2023-02-27 22:09:10 +0100

    `✨UPDATE` Added Type to Card UI

--- app/src/main/java/dev/xero/amphibians/ui/theme/Color.kt
+++ app/src/main/java/dev/xero/amphibians/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package dev.xero.amphibians.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Beige = Color(0xFFAD8F74)
 val DeepGreen = Color(0xFF4B9F86)
 val MudGreen = Color(0xFF7FA38B)
 val DeepBlue = Color(0xFF4B5A5E)
